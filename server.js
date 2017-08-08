59c5cee35dacda10ec70bae1ec161ec87d38c514
Israel Bernal <imbernal9203@gmail.com>
2017-08-08 16:32:39 -0700

    initial 2

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+app.listen(9203, ()=>{
+  console.log("Testing");
+});
